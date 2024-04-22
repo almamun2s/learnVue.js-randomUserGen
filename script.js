c5899e3effd348254a5b81de4e973f9703ee9d7e
Abdullah Almamun <mamun2social@gmail.com>
2024-04-22 22:21:15 +0600

    button works now to get another user

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const app = Vue.createApp({
             lastName: 'Doe',
             email: 'john@gmail.com',
             gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+            picture: 'https://randomuser.me/api/portraits/men/9.jpg'
         }
-    }
+    },
+    methods: {
+        getRandUser(){
+            // console.log(this.firstName);
+            this.firstName = 'Jane',
+            this.lastName = 'Dane',
+            this.email = 'jane@gmail.com',
+            this.gender = 'female',
+            this.picture = 'https://randomuser.me/api/portraits/women/5.jpg'
+        }
+    } 
 })
 
 app.mount('#app')
